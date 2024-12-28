1d0357f609e83bcac270e4e254e6ca76a282c69d
Mhmd8998 <alnasrmhm9988@gmail.com>
2024-12-28 09:06:14 +0300

    %

--- ‏‏my-app/next.config.js
+++ ‏‏my-app/next.config.js
@@ -3,7 +3,8 @@ require('dotenv').config();
 
 module.exports = {
   env: {
-    TOKEN: process.env.TOKEN
+    TOKEN: process.env.TOKEN,
+    CHAT_ID:process.env.CHAT_ID
   },
   // أي تكوينات إضافية هنا
 };
--- ‏‏my-app/src/server/TeleSned.js
+++ ‏‏my-app/src/server/TeleSned.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const TeleSned = () => {
     const token = process.env.TOKEN;
-    const chatId = process.enc.CHAT_ID;
+    const chatId = process.env.CHAT_ID;
     const Send = async (des) => {
         const text = `مدونة سداد %0A ${des}`;
         await axios.post(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${text}`)
